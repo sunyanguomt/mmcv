e4a2b3d3779eb679ac1c538bb95f4f1aa6d17ddf
sunyanguomt <yanguo.sun@mthreads.com>
2025-01-16 10:25:30 +0800

    Update build_musa.sh

--- build_musa.sh
+++ build_musa.sh
@@ -1,4 +1,1 @@
-export CFLAGS="-g -O0"
-export CPPFLAGS="-DDEBUG=1"
-
-DEBUG=1 MMCV_WITH_OPS=1 MUSA_ARCH=22 FORCE_MUSA=1 python setup.py install
+MMCV_WITH_OPS=1 MUSA_ARCH=22 FORCE_MUSA=1 python setup.py install
